d4b02209ccf54d38b174eff706568874cad115d0
Matías Rivera <juanmatiasrivera@outlook.com>
2020-10-13 12:21:15 -0300

    Added handle errors middlewares

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
-
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
@@ -11,12 +11,21 @@ connectDB()
 
 const app = express()
 
+
+
 app.get('/', (req,res) => {
     res.send('API is running...')
 });
 
 //routes
 app.use('/api/products',productRoutes)
+
+//middlewares
+app.use(notFound)
+app.use(errorHandler)
+
+
+
 
 const PORT = process.env.PORT || 5000
 
