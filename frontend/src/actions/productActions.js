42a45d77c8f15772a1328876af881bee9805592c
Matías Rivera <juanmatiasrivera@outlook.com>
2020-10-13 14:04:59 -0300

    Added Message and Loader components

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,11 +1,12 @@
 import axios from 'axios'
 import {PRODUCT_LIST_REQUEST,PRODUCT_LIST_SUCCESS,PRODUCT_LIST_FAIL} from '../constants/productConstants'
 
-
+//get products list
 export const listProducts = () =>  async (dispatch) => { 
     try{
         dispatch({type: PRODUCT_LIST_REQUEST})
-
+        
+        //api call to get all products
         const {data} = await axios.get('/api/products')
         dispatch({
             type: PRODUCT_LIST_SUCCESS,
