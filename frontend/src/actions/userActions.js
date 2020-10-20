c4b5fcc1aea05837fb42ad92088ea65ed5c5dc53
Matías Rivera <juanmatiasrivera@outlook.com>
2020-10-20 13:53:55 -0300

    Added user authentication in frontend
    Modified userActions and Header component
    Added LoginScreen

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,16 +1,21 @@
 import axios from 'axios'
-import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL } from './../constants/userConstants';
+import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT } from './../constants/userConstants';
+
+
 export const login = (email, password) => async(dispatch) => {
     try{
         dispatch({
             type: USER_LOGIN_REQUEST
         })
+
+        //headers
         const config = {
             headers: {
                 'Content-Type':'application/json'
             }
         }
 
+        //get login data
         const {data} = await axios.post(
             '/api/users/login',
             {email, password},
@@ -20,6 +25,8 @@ export const login = (email, password) => async(dispatch) => {
             type: USER_LOGIN_SUCCESS,
             payload: data
         })
+
+        //set user info in local storage
         localStorage.setItem('userInfo', JSON.stringify(data))
 
     }catch(error){
@@ -31,4 +38,9 @@ export const login = (email, password) => async(dispatch) => {
                     : error.message
         })
     }
+}
+
+export const logout = () => (dispatch) => {
+    localStorage.removeItem('userInfo')
+    dispatch({ type: USER_LOGOUT })
 }
