3ffe89c86c0fb61157ca7d5d181928be6dde6b65
Matías Rivera <juanmatiasrivera@outlook.com>
2020-10-20 22:05:29 -0300

    added Shipping screen

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import {CART_ADD_ITEM, CART_REMOVE_ITEM} from '../constants/cartConstants'
+import {CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS} from '../constants/cartConstants'
 
 export const addToCart = (id, qty) => async(dispatch, getState) => {
     //get item data
@@ -21,10 +21,20 @@ export const addToCart = (id, qty) => async(dispatch, getState) => {
     localStorage.setItem(`cartItems`, JSON.stringify(getState().cart.cartItems))
 }
 
+//remove item from cart
 export const removeFromCart = (id) => (dispatch, getState) => {
     dispatch({
         type: CART_REMOVE_ITEM,
         payload: id
     })
     localStorage.setItem('cartItems',JSON.stringify(getState().cart.cartItems))
+}
+
+//save shipping address in cart
+export const saveShippingAddress = (data) => (dispatch) => {
+    dispatch({
+        type: CART_SAVE_SHIPPING_ADDRESS,
+        payload: data
+    })
+    localStorage.setItem('shippingAddress',JSON.stringify(data))
 }
