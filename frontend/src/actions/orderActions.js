27d04a7ccf9de2e8b499a42d9f4a75978d0a6ade
Matías Rivera <juanmatiasrivera@outlook.com>
2020-10-21 17:28:41 -0300

    Added order screen

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -2,7 +2,10 @@ import axios from 'axios'
 import {
     ORDER_CREATE_REQUEST,
     ORDER_CREATE_SUCCESS,
-    ORDER_CREATE_FAIL
+    ORDER_CREATE_FAIL,
+    ORDER_DETAILS_REQUEST,
+    ORDER_DETAILS_SUCCESS,
+    ORDER_DETAILS_FAIL
 } from '../constants/orderConstants'
 
 
@@ -44,3 +47,45 @@ export const createOrder = (order) => async(dispatch, getState) => {
         })
     }
 }
+
+
+//get order details 
+export const getOrderDetails = (id) => async(dispatch, getState) => {
+    try{
+        dispatch({
+            type: ORDER_DETAILS_REQUEST
+        })
+
+        //get user from state
+        const {userLogin: {userInfo}} = getState()
+        //headers
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        //create order request
+        const {data} = await axios.get(
+            `/api/orders/${id}`,
+            config
+            )
+        dispatch({
+            type: ORDER_DETAILS_SUCCESS,
+            payload: data
+        })
+
+    }catch(error){
+        dispatch({
+            type: ORDER_DETAILS_FAIL,
+            payload: 
+                error.response && error.response.data.message
+                    ? error.response.data.message 
+                    : error.message
+        })
+    }
+}
+
+
+
+
