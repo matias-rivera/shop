080ba4df149f9e4623594680e9bab19fcacb68bc
Matías Rivera <juanmatiasrivera@outlook.com>
2020-10-22 15:49:50 -0300

    added admin middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -41,6 +41,17 @@ const protect = asyncHandler(async(req, res, next) => {
     
 })
 
+const admin = (req, res, next) => {
+    if(req.user && req.user.isAdmin){
+        next()
+    }else{
+        res.status(401)
+        throw new Error('Not authorized, admin only')
+    }
+
+}
+
 export {
-    protect
+    protect,
+    admin
 }
