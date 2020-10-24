e6d14449d8e1ed61937050c8a20198d2310e5e41
Matías Rivera <juanmatiasrivera@outlook.com>
2020-10-24 07:12:24 -0300

    added search box in home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,6 +39,7 @@ const App = () => {
           <Route path='/login' component={LoginScreen}/>
           <Route path='/product/:id' component={ProductScreen}/>
           <Route path='/cart/:id?' component={CartScreen}/>
+          <Route path='/search/:keyword' component={HomeScreen}/>
           <Route path='/' component={HomeScreen} exact/>
         </Container>
       </main>
--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -21,12 +21,12 @@ import {
 } from '../constants/productConstants'
 
 //get products list
-export const listProducts = () =>  async (dispatch) => { 
+export const listProducts = (keyword = '') =>  async (dispatch) => { 
     try{
         dispatch({type: PRODUCT_LIST_REQUEST})
         
         //api call to get all products
-        const {data} = await axios.get('/api/products')
+        const {data} = await axios.get(`/api/products?keyword=${keyword}`)
         dispatch({
             type: PRODUCT_LIST_SUCCESS,
             payload: data
